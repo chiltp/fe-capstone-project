10bb15a5f2f7e210e5344e79e2fbffe1a1357884
chiltp <luongphuongchi1505@gmail.com>
2025-06-27 09:14:29 +0700

    new project is initialized

---
+++ test.js
@@ -0,0 +1,34 @@
+class Pastry {
+    constructor(name = "Generic Pastry", flavor = "Plain", price = 5){
+        this.name = name;
+        this.flavor = flavor;
+        this.price = price;
+    }
+    describe() {
+        console.log(`This is a ${this.flavor}-flavored ${this.name} with only ${this.price}.`)
+    }
+}
+class PainAuChocolat extends Pastry {
+    constructor(name, flavor, price, chocolateBars = 2, isWarm = false){
+        super(name, flavor, price)
+        this.chocolateBars = chocolateBars;
+        this.isWarm = isWarm;
+    }
+    warmUp() {
+        this.isWarm = true;
+    }
+    addChocolate() {
+        this.chocolateBars += 1;
+    }
+    describe() {
+        const warm = this.isWarm ? "Warm" : "Cold";
+        console.log(`${warm} ${this.name} with ${this.chocolateBars} bars of chocolate. Price: ${this.price}.`)
+    }
+}
+function serveBreakfastPastry(){
+    const pastry = new PainAuChocolat("Pain au Chocolat", "Chocolate", 6)
+    pastry.warmUp();
+    pastry.addChocolate();
+    pastry.describe();
+}
+serveBreakfastPastry()
